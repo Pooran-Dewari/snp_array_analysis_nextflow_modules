e3d28e19bfade8e88d6a1a78fd6ad91651b65281
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2024-01-03 13:03:42 +0000

    Update qsub_sam2bam.sh

--- convert_sam2bam/qsub_sam2bam.sh
+++ convert_sam2bam/qsub_sam2bam.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-#!/bin/sh
-
 #$ -V -cwd
 #$ -l h_rt=24:00:00 ###HH:MM:SS
 #$ -l h_vmem=20G
 #$ -pe sharedmem 16
-#$ -P roslin_macqueen_lab
 
 # make sure you are using recent version of samtools to avoid issues with -o option
 module load igmm/apps/samtools/1.16.1
