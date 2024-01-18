3584725faacc730552a1692454db387de8e07424
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2024-01-18 16:06:32 +0000

    Update qsub_mpileup.sh

--- mpileup/qsub_mpileup.sh
+++ mpileup/qsub_mpileup.sh
@@ -8,12 +8,12 @@
 module load igmm/apps/samtools/1.16.1
 module load roslin/java/18.0.2
 
-cd /exports/eddie/scratch/pdewari/hamour/results/bams/RG
+cd /exports/eddie/scratch/pdewari/hamour/results/bams/RG/dup_removed
 
-# make sure you have bamfiles.txt file in .../bams/RG dir
+# make sure you have bamfiles.txt file in <.../bams/RG/dup_removed> dir
 # bamfiles.txt files is attached, essentially it's one bam file per line
 
 samtools mpileup -q 15 -Q 0 -d 8000 -R -A -B \
-	-f /exports/eddie/scratch/pdewari/hamour/genome/fEpiCoi_cnag1_curated_primary.no_mt.fa \
-	-b ./bamfiles.txt \
-	-o hamour_all.mpileup
+        -f /exports/eddie/scratch/pdewari/hamour/genome/fEpiCoi_cnag1_curated_primary.no_mt.fa \
+        -b ./bamfiles.txt \
+        -o hamour_all.mpileup
