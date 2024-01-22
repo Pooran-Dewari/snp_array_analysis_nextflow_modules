54921e7de260b568eba34bf4929a9a79ddc825e8
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2024-01-22 13:47:00 +0000

    Update mpileup and output dir location

--- 08_poolsnp/qsub_poolsnp.sh
+++ 08_poolsnp/qsub_poolsnp.sh
@@ -4,13 +4,14 @@
 #$ -l h_rt=60:00:00 ###HH:MM:SS
 #$ -l h_vmem=60G
 #$ -pe sharedmem 4
+#$ -P roslin_macqueen_lab
 
 module load igmm/apps/parallel/20230522
 module load igmm/apps/python/3.10.6
 
-bash /exports/eddie/scratch/pdewari/hamour/results/bams/RG/PoolSNP.sh \
-mpileup=/exports/eddie/scratch/pdewari/hamour/results/bams/RG/hamour_all.mpileup \
-output=/exports/eddie/scratch/pdewari/hamour/results/bams/RG/hamour_final \
+bash /exports/eddie/scratch/pdewari/hamour/results/bams/RG/dup_removed/PoolSNP.sh \
+mpileup=/exports/eddie/scratch/pdewari/hamour/results/bams/RG/dup_removed/hamour_all.mpileup \
+output=/exports/eddie/scratch/pdewari/hamour/results/bams/RG/dup_removed/hamour_final \
 reference=/exports/eddie/scratch/pdewari/hamour/genome/fEpiCoi_cnag1_curated_primary.no_mt.fa \
 names=hamour_alwakra,hamour_aqua,hamour_doha,hamour_shamal \
 max-cov=0.95 \
