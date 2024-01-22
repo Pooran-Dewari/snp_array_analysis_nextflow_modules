9138d6318f2e02a19336d93159d2f265d18fc790
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2024-01-22 13:48:17 +0000

    add mkdir for outputting

--- 09_vcftool/qsub_vcftools.sh
+++ 09_vcftool/qsub_vcftools.sh
@@ -4,12 +4,14 @@
 #$ -l h_rt=00:10:00 ###HH:MM:SS
 #$ -l h_vmem=60G
 #$ -pe sharedmem 4
+#$ -P roslin_macqueen_lab
 
 module load igmm/apps/vcftools/0.1.13
 
-VCF=/exports/eddie/scratch/pdewari/hamour/results/bams/RG/poolsnp_results/hamour_final.vcf.vcf.gz
-OUTPUT=/exports/eddie/scratch/pdewari/hamour/results/bams/RG/poolsnp_results
+mkdir /exports/eddie/scratch/pdewari/hamour/results/bams/RG/dup_removed/poolsnp_results
 
+VCF=/exports/eddie/scratch/pdewari/hamour/results/bams/RG/dup_removed/hamour_final.vcf.gz
+OUTPUT=/exports/eddie/scratch/pdewari/hamour/results/bams/RG/dup_removed/poolsnp_results
 
 vcftools --gzvcf $VCF --min-alleles 2 --max-alleles 2 --recode --out $OUTPUT/hamour_vcf_biallelic
 
